55fe4efd8d1dc9c3981e87b906f038a48f67076c
duynv1895 <duynv@ql6625.fr>
2021-01-17 20:08:21 +0700

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,26 @@
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class TasksService {
+
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDto) {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+        this.tasks.push(task);
+        return this.tasks;
+    }
+}
